a9bb58d0401ad57f08bc68cff816344b0fe8f1c0
orchi <orchid345617@gmail.com>
2020-05-22 13:37:45 +0300

    added the generator() function

--- RPG.py
+++ RPG.py
@@ -7,10 +7,14 @@ import time
 knight_hp = 10
 monsters_count: int = 0
 knight_strength: int = 10
-border_low: int = 5
-border_high: int = 30
+monster_border_low: int = 5
+monster_border_high: int = 30
 events_border_low: int = 1
 events_border_high: int = 3
+apple_border_low: int = 5
+apple_border_high: int = 15
+sword_border_low: int = 10
+sword_border_high: int = 30
 current_strength: int = knight_strength
 current_hp: int = knight_hp
 delay = 2
@@ -20,14 +24,25 @@ print('Ваше здоровье и сила атаки равна: %s, %s'
       % (current_hp, current_strength))
 time.sleep(delay)
 
-while monsters_count < 10:
-    event: int = random.randint(events_border_low, events_border_high)
 
-    '''Ниже начинается блок кода, связанный с монстром.
-     У игрока два варианта: 1 Сразиться с монстром, 2 убежать'''
-    if event == 1:
-        monster_hp: int = random.randint(border_low, border_high)
-        monster_strength: int = random.randint(border_low, border_high)
+def generator(type):
+    if type == 1:  # событие
+        return random.randint(events_border_low, events_border_high)
+    elif type == 2:
+        return random.randint(monster_border_low, monster_border_high)  # монстр
+    elif type == 3:
+        return random.randint(apple_border_low, apple_border_high)  # яблоко
+    elif type == 4:
+        return random.randint(sword_border_low, sword_border_high)  # меч
+
+
+while monsters_count < 10:
+
+    if generator(1) == 1:
+        '''Ниже начинается блок кода, связанный с монстром.
+             У игрока два варианта: 1 Сразиться с монстром, 2 убежать'''
+        monster_hp: int = generator(2)
+        monster_strength: int = generator(2)
         print('Вы встретили чудовище с %s жизнями и %s силой удара.'
               % (monster_hp, monster_strength))
         print('Нажмите 1 чтобы атаковать, нажмите 2 чтобы убежать')
@@ -48,6 +63,7 @@ while monsters_count < 10:
                           % current_hp)
                     monsters_count += 1
                     time.sleep(delay)
+
                     break
             elif user_input == '2':
                 print('Вы убежали от чудовища')
@@ -56,19 +72,19 @@ while monsters_count < 10:
             else:
                 print('Вы выбрали неверный вариант.Попробуйте еще раз')
 
-    '''Ниже начинается блок кода, связанный с нахождением яблока.'''
-    if event == 2:
-        apple_health: int = random.randint(border_low, border_high)
+    if generator(1) == 2:
+        '''Ниже начинается блок кода, связанный с нахождением яблока.'''
+        apple_health: int = generator(3)
         print('Вы нашли яблочко, которое увеличивает ваше здоровье на %s'
               % apple_health)
         current_hp = current_hp + apple_health
         print('Ваше текущее здоровье равно %s' % current_hp)
         time.sleep(delay)
 
-    '''Ниже начинается блок кода, связанный с нахождением меча.
-         У игрока два варианта: 1 Взять меч, 2 Пройти мимо'''
-    if event == 3:
-        sword_strength: int = random.randint(border_low, border_high)
+    if generator(1) == 3:
+        '''Ниже начинается блок кода, связанный с нахождением меча.
+                 У игрока два варианта: 1 Взять меч, 2 Пройти мимо'''
+        sword_strength: int = generator(4)
         print('Вы нашли меч с силой атаки %s' % sword_strength)
         print('Нажмите 1, чтобы взять его, нажмите 2, чтобы пройти мимо')
         while True:
